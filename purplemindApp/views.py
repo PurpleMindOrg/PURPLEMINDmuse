a8ccf5d4090e33216ee1cbbed025680a549c2f3d
jackleonard <jck.leonard@gmail.com>
2017-11-17 00:11:21 +0000

    ## PURPLEMIND App 0.0.3.2.2 Alpha
    # Added half signup form [ not linked ]

--- purplemindApp/views.py
+++ purplemindApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
@@ -9,3 +13,20 @@ from django.http import HttpResponse
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
+
+    def HomePageView(request):
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+                login(request, user)
+                return redirect('home')
+        else:
+            form = UserCreationForm()
+        return render(request, 'index.html', {'form': form})
+
+# Sign up controller #
+
